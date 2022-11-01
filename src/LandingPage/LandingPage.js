954662250a07c66ff3e68f934442a355f824a7c1
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-11-01 12:59:46 +0700

    yes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,12 +3,13 @@ import './LandingPage.css'
 function LandingPage(){
 return(
     <div className="topnav">
-        
+        <img src='Group 5.png' alt='logo' className='logo'></img> 
+        <div class="object-none object-center">
         <a className="active" href="#HomePage">Home Page</a>
         <a href="#Menu">Menu</a>
         <a href="#Promo">Promo</a>
         <a href="#Location">Location</a>
-        <img src='Group 5.png' alt='logo' className='logo'></img>   
+          </div>
     </div>
 )
 }
