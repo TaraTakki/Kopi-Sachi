91c1f14bfefc46bc2382d4aedf5c766ecf33c63c
arjunazaki <arjunazaki93@gmail.com>
2022-11-01 11:54:39 +0700

    yes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import LandingPage from './LandingPage/LandingPage';
+import LoginPage from './LoginPage/LoginPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <LoginPage />
   </React.StrictMode>
 );
 
