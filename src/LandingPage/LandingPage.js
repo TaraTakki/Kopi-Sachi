6effe6b07329a23b63dbfa2ddbc2d74b37923bc2
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-11-01 12:32:57 +0700

    yes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,22 +3,14 @@ import './LandingPage.css'
 function LandingPage(){
 return(
     <div className="topnav">
+        
         <a className="active" href="#HomePage">Home Page</a>
         <a href="#Menu">Menu</a>
         <a href="#Promo">Promo</a>
         <a href="#Location">Location</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()"></a>
-        <i class="fa fa-bars"></i>
+        <img src='Group 5.png' alt='logo' className='logo'></img>   
     </div>
 )
 }
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
 
 export default LandingPage;
