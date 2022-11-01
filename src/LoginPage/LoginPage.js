c6aaca8a25277ab6a915d12831586fabebf8db99
arjunazaki <arjunazaki93@gmail.com>
2022-11-01 15:05:10 +0700

    yes

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,8 +1,33 @@
+import React from "react";
 
 function LandingPage(){
     return(
-       
-        <div><img src='background.png' alt='background' className='background'></img>   </div>
+        <div  className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+           
+            
+        <div  className=' bg-[#F6EBDA] flex flex-col justify-center items-center'>
+            <section className="flex justify-center"><img src="Group 13.png"/></section>
+            <form>
+          <img src="Login.png"/>
+                <div>
+                    <label>Email</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+                <button>Login</button>
+                
+            </form>
+
+        </div>
+        <div className='hidden sm:block'>
+                <img className='w-full h-full object-cover' src="background.png" alt=""/>
+            </div>
+        
+        
+        </div>
     )
     }
 
