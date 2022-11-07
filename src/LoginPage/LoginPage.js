5cfc4fa0b1e3de9e7d7d691f193e11a0a7744a99
arjunazaki <arjunazaki93@gmail.com>
2022-11-07 13:11:35 +0700

    yes

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './LoginPage.css';
 import {useContext} from 'react';
-import { Link, useNavigate } from ' react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AppContext } from '../App';
 
 
@@ -29,7 +29,7 @@ function LandingPage(){
     alert('Login Gagal. username atau password salah!');
     else
     {
-        Context.setPengguna(cek_login); 
+        Context.setPengguna(cek_login);
         navigasi('/');
     }
     }
