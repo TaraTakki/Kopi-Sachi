be226eda0f55930a5ba422f6f9c2f07642b8b164
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-11-07 07:32:16 +0700

    yes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ return(
             <section>
                 <img src='Group 5.png' alt='logo' className='Logo'></img>
             </section>
-    <section className='flex justify-center'><div className='navbar'>
+    <section className='flex justify-center my-3'><div className='navbar'>
     <ul>
     <li><a href="#">Home Page</a></li>
     <li><a href="#">Menu</a></li>
@@ -18,10 +18,10 @@ return(
     </section>
     <div 
     className='flex justify-end mr-8 -my-8 mx'>
-        <button class="bg-transparent hover:bg-yellow-700 text-yellow-700 font-semibold hover:text-white py-2 px-4 border border-yellow-700 hover:border-transparent rounded">
+        <button class="bg-transparent hover:bg-yellow-700 text-yellow-700 font-semibold hover:text-white py-2 px-4 border-2 border-yellow-700 hover:border-transparent rounded-full ">
   Sign Up
 </button>
-<button class="bg-transparent hover:bg-yellow-700 text-yellow-700 font-semibold hover:text-white py-2 px-4 border border-yellow-700 hover:border-transparent rounded">
+<button class="bg-transparent hover:bg-yellow-700 text-yellow-700 font-semibold hover:text-white py-2 px-4 border-2 border-yellow-700 hover:border-transparent rounded-full">
   Log In
 </button>
     </div>
@@ -31,6 +31,15 @@ return(
       <div className='tagline'>
       <p>Enjoy your coffee</p>
       <p>before your activity</p>
+      </div>
+      <div className='desc'>
+      <p>Boost your productivity and build your mood</p>
+      <p>with a glass of coffee in the morning</p>
+      </div>
+      <div className='flex justify-start ml-20 my-5'>
+      <button class="bg-yellow-900 hover:bg-yellow-600 text-orange-300 font-semibold hover:text-white py-2 px-4 border-5 border-red-900 hover:border-transparent rounded-full">
+  More menu
+</button>
       </div>
     </div>
     </body>
