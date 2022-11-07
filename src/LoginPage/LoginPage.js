eecf71f73a9aafe9385b67ce89e804097f7e78b0
arjunazaki <arjunazaki93@gmail.com>
2022-11-07 09:34:40 +0700

    yes

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,7 +1,38 @@
 import React from "react";
-import './LoginPage.css'
+import './LoginPage.css';
+import {useContext} from 'react';
+import { Link, useNavigate } from ' react-router-dom';
+import { AppContext } from '../App';
+
 
 function LandingPage(){
+    const Context = useContext(AppContext);
+    let navigasi = useNavigate();
+    const loginClick = async(e)=> {e.preventDefault();
+    let u = e.target.email.value;
+    let p = e.target.password.value;
+    let cek_login = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          
+          username: u,
+          password: p,
+          // expiresInMins: 60, // optional
+        })
+      })
+      .then(res => res.json())
+      .then(hasil =>{return hasil});
+    // alert('Email: '+u+ '\nPassword: '+p)
+
+    if(cek_login.username === undefined)
+    alert('Login Gagal. username atau password salah!');
+    else
+    {
+        Context.setPengguna(cek_login); 
+        navigasi('/');
+    }
+    }
     return(
         <div  className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full '>
            
@@ -9,17 +40,17 @@ function LandingPage(){
         <div  className=' bg-[#F6EBDA] '>
             <section className="flex flex-col items-center mt-16"><img src="Group 13.png" className="responsive"/></section>
             
-           <div className="  "><form className="">
+           <div className="  "><form onSubmit={loginClick} className="">
           
             <div className="flex flex-col items-center"><h1 className="text-[#57170D] title text-xl font-semibold ">Login</h1></div>
                 <div className="flex flex-col items-center ">
                     <label></label>
-                    <input className='w-6/12 txtbgcolor rounded-lg bg-[#F8D8A9] mt-3 p-1 px-3 title font-semibold ' type="email" placeholder="Email"  />
+                    <input className=' w-6/12 txtbgcolor rounded-lg bg-[#F8D8A9] mt-3 p-1 px-3 title font-semibold ' type="text" placeholder="Email" name="email" id="idEmail"  />
                 </div>
 
                 <div className="flex flex-col items-center ">
                     <label></label> 
-                    <input className=' w-6/12 rounded-lg mt-5 p-1 txtbgcolor px-3  placeholder title font-semibold bg-[#F8D8A9] ' type="password" placeholder="Password" />
+                    <input className=' w-6/12 rounded-lg mt-5 p-1 txtbgcolor px-3  placeholder title font-semibold bg-[#F8D8A9] ' type="password" placeholder="Password" name="password" id="idPass" />
                 </div>
 
                 <div className="flex flex-col items-center mt-5 "><button class="font-semibold w-6/12 h-10 px-5 m-2 text-[#F6EBDA] transition-colors duration-150 bg-[#433416] rounded-lg focus:shadow-outline hover:bg-[#433416] title">Submit</button></div>
@@ -31,7 +62,7 @@ function LandingPage(){
         <div className='hidden sm:block'>
             
                 <img className='w-full h-full object-cover logosachi2' src="background.png" alt=""/>
-                <img className="logosachi w-6/12 h-6/12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  " src="Group 5.png" alt=""/>
+                <section className="flex justify-center"><img className="logosachi w-6/12 h-6/12 " src="Group 5.png" alt=""/></section>
             </div>
 
             
